0f99e9a8f4d5859e7cfbee7fe827b2fdb741c533
AlphaNeon <102443376+ItsAlphaNeon@users.noreply.github.com>
2023-09-16 06:58:16 -0600

    Implemented auto update system for future changes

--- autopilot.plugin.js
+++ autopilot.plugin.js
@@ -2,7 +2,7 @@
  * @name Autopilot
  * @author AlphaNeon
  * @description A plugin that uses the OpenAI API to automatically reply to DM's you select. Very experimental, and just for fun.
- * @version 0.0.4
+ * @version 0.0.5
  */
 
 class APTools {
@@ -163,7 +163,10 @@ class QuerySelectorPresets {
 }
 
 module.exports = class Autopilot {
+
   constructor() {
+    const pluginVersion = "0.0.5";
+
     // Default settings
     this.autopilotActive = false;
     this.Settings = {
@@ -200,12 +203,93 @@ module.exports = class Autopilot {
     this.MessagesArray = [];
   }
 
+  checkForUpdates() {
+    const githubFileURL = "https://raw.githubusercontent.com/ItsAlphaNeon/AutoPilot-for-BetterDiscord/main/autopilot.plugin.js";
+    const localVersion = this.pluginVersion;
+
+    try {
+      fetch(githubFileURL)
+        .then((response) => response.text())
+        .then((text) => {
+          let remoteVersion = text.match(/@version\s+(\S+)/)[1];
+          if (localVersion !== remoteVersion || localVersion < remoteVersion) {
+            BdApi.UI.showConfirmationModal(
+              "Autopilot",
+              "A new version of Autopilot is available! Would you like to update now?",
+              {
+                confirmText: "Update",
+                cancelText: "No Thanks",
+                onConfirm: () => {
+                  // Download the new version
+                  BdApi.showToast("Autopilot is updating...", { type: "info" });
+                
+                  fetch(githubFileURL)
+                    .then((response) => response.text())
+                    .then((text) => {
+                      // Write the new version to the local file
+                      const fs = require("fs");
+                      const path = require("path");
+                
+                      const filePath = BdApi.Plugins.folder + "/autopilot.plugin.js";
+                      fs.writeFile(filePath, text, (err) => {
+                        if (err) {
+                          console.error(err);
+                          BdApi.showToast("Failed to update Autopilot", { type: "error" });
+                        } else {
+                          BdApi.showToast("Autopilot has been updated", { type: "success" });
+                          BdApi.showConfirmationModal(
+                            "Update Successful",
+                            "Please reload Discord to apply the changes.",
+                            {
+                              confirmText: "Reload",
+                              onCancel: () => {
+                                console.log("Discord reload canceled");
+                              },
+                              onConfirm: () => {
+                                // Reload Discord after the update
+                                BdApi.Plugins.disable(BdApi.Plugins.get("Autopilot").name);
+                                BdApi.Plugins.enable(BdApi.Plugins.get("Autopilot").name);
+                              },
+                            }
+                          );
+                        }
+                      });
+                    })
+                    .catch((error) => {
+                      console.log(error);
+                    });
+                },
+                
+                onCancel: () => {
+                  // Don't download the new version
+                  BdApi.showToast("Autopilot will not be updated.", {
+                    type: "info",
+                  
+                }
+              );
+            }
+          }
+            )
+          }
+          else {
+            console.log("Autopilot is up to date!"); // debug
+          }
+        });
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+
   start() {
     // Load settings if they don't exist
     if (!BdApi.Data.load("Autopilot", "settings")) {
       this.saveSettings();
     }
     this.loadSettings();
+
+    // Check for updates
+    this.checkForUpdates();
 
     // Check if first run
     if (this.Settings.IsFirstRun) {
